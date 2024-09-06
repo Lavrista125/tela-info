a1a8b891592d19971264a682cc10f1fcfc7dca31
Vitoria <vitoriasouzalavrista@gmail.com>
2024-09-06 18:37:58 -0300

    tela-info

---
+++ info.js
@@ -0,0 +1,18 @@
+const images = document.querySelectorAll('.imagens img');
+
+let currentIndex = 0;
+
+/* trocar imagem em sequencia */
+function showNextImage() {
+
+    images[currentIndex].classList.remove('active');
+    
+   /* ciclo das imagens */
+    currentIndex = (currentIndex + 1) % images.length;
+    
+    images[currentIndex].classList.add('active');
+}
+
+images[currentIndex].classList.add('active');
+
+setInterval(showNextImage, 3000);
